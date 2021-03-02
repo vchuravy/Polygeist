4255dd0f60876059bb42dfcdd3a68652be0c369c
lorenzo chelini <l.chelini@tue.nl>
2021-03-02 16:34:46 +0100

    start to raise (something)

--- mlir/lib/Dialect/StandardOps/IR/Ops.cpp
+++ mlir/lib/Dialect/StandardOps/IR/Ops.cpp
@@ -8,6 +8,7 @@
 
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/CommonFolders.h"
 #include "mlir/Dialect/StandardOps/Utils/Utils.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
@@ -1343,10 +1344,9 @@ struct SimplfyIntegerCastMath : public OpRewritePattern<IndexCastOp> {
       setLocationAfter(b2, iadd.getOperand(1));
       rewriter.replaceOpWithNewOp<AddIOp>(
           op,
-          b.create<IndexCastOp>(op.getLoc(), iadd.getOperand(0),
-                                       op.getType()),
+          b.create<IndexCastOp>(op.getLoc(), iadd.getOperand(0), op.getType()),
           b2.create<IndexCastOp>(op.getLoc(), iadd.getOperand(1),
-                                       op.getType()));
+                                 op.getType()));
       return success();
     }
     if (auto iadd = op.getOperand().getDefiningOp<SubIOp>()) {
@@ -1356,10 +1356,9 @@ struct SimplfyIntegerCastMath : public OpRewritePattern<IndexCastOp> {
       setLocationAfter(b2, iadd.getOperand(1));
       rewriter.replaceOpWithNewOp<SubIOp>(
           op,
-          b.create<IndexCastOp>(op.getLoc(), iadd.getOperand(0),
-                                       op.getType()),
+          b.create<IndexCastOp>(op.getLoc(), iadd.getOperand(0), op.getType()),
           b2.create<IndexCastOp>(op.getLoc(), iadd.getOperand(1),
-                                       op.getType()));
+                                 op.getType()));
       return success();
     }
     if (auto iadd = op.getOperand().getDefiningOp<MulIOp>()) {
@@ -1369,10 +1368,9 @@ struct SimplfyIntegerCastMath : public OpRewritePattern<IndexCastOp> {
       setLocationAfter(b2, iadd.getOperand(1));
       rewriter.replaceOpWithNewOp<MulIOp>(
           op,
-          b.create<IndexCastOp>(op.getLoc(), iadd.getOperand(0),
-                                       op.getType()),
+          b.create<IndexCastOp>(op.getLoc(), iadd.getOperand(0), op.getType()),
           b2.create<IndexCastOp>(op.getLoc(), iadd.getOperand(1),
-                                       op.getType()));
+                                 op.getType()));
       return success();
     }
     return failure();
